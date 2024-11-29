781f27df67d22aa93e8e9e97aec8c901e4b1f2b4
LuiggiC53 <99456683+LuiggiC53@users.noreply.github.com>
2024-11-28 19:41:19 -0500

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ValidarPasswordApp',
+  webDir: 'www'
+};
+
+export default config;
